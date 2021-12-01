4c5aa6f245d70f6a2e58bfc04c1a354cfe494aad
alexei <ageneralov@iiko.ru>
2021-12-01 11:54:40 +0300

    initial

---
+++ src/AppBundle/Controller/OAuth2Controller.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use iikoExchangeBundle\Contract\Connection\OAuth2ConnectionInterface;
+use iikoExchangeBundle\Contract\Exchange\ExchangeInterface;
+use iikoExchangeBundle\Contract\Service\ExchangeConfigStorageInterface;
+use iikoExchangeBundle\Exception\ConfigNotFoundException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OAuth2Controller extends Controller
+{
+	/**
+	 * @Route("/api/exchange/oauth2/goto")
+	 * @Method("GET")
+	 */
+	public function getOAuth2Uri(Request $request)
+	{
+		$exchangeSkeleton = $this->get('exchange.directory')->getExchangeByCode($request->query->get('exchangeCode'));
+		$exchangeSkeleton->setId($request->query->getInt('exchangeId', 1));
+		/** @var ExchangeConfigStorageInterface $exchangeConfigStorage */
+		$exchangeConfigStorage = $this->get('exchange.storage.config');
+
+		$type = $request->query->get('type', 'loader');
+		/** @var OAuth2ConnectionInterface $node */
+		$node = $type === 'loader' ? $exchangeSkeleton->getLoader() : $exchangeSkeleton->getExtractor();
+
+		$redirectUri = $this->getRedirectUrl($request, $exchangeSkeleton, $type);
+
+		$exchangeConfigStorage->appendConfiguration($exchangeSkeleton, $node, $node::CONFIG_REDIRECT_URI, $redirectUri);
+
+		$config = $exchangeConfigStorage->getConfiguration($exchangeSkeleton, $type === 'loader' ? $exchangeSkeleton->getLoader() : $exchangeSkeleton->getExtractor());
+		$node->setConfigCollection($config);
+
+
+		return $this->redirect($node->getRedirectToLoginUrl($config[OAuth2ConnectionInterface::CONFIG_CLIENT_ID], $redirectUri));
+	}
+
+	private function getRedirectUrl(Request $request, ExchangeInterface $exchange, string $type)
+	{
+		return sprintf("%s%s",
+			str_replace([$request->getPathInfo(), $request->getQueryString(), "?"], "", $request->getUri()),
+			$this->generateUrl("Exchange.RedirectUri", ['exchangeCode' => $exchange->getCode(), 'exchangeId' => $exchange->getId(), 'type' => $type])
+		);
+	}
+
+	/**
+	 * @Route("/api/exhange/oauth2/auth/{exchangeCode}/{exchangeId}/{type}", name="Exchange.RedirectUri")
+	 * @Method("GET")
+	 */
+	public function redirectUri(Request $request, string $exchangeCode, int $exchangeId, string $type)
+	{
+		$exchangeSkeleton = $this->get('exchange.directory')->getExchangeByCode($exchangeCode);
+		$exchangeSkeleton->setId($exchangeId);
+
+		if ($request->query->has('error'))
+		{
+			return new JsonResponse($request->query->get('error'));
+		}
+
+		$code = $request->query->get('code');
+
+		/** @var ExchangeConfigStorageInterface $exchangeConfigStorage */
+		$exchangeConfigStorage = $this->get('exchange.storage.config');
+
+		/** @var OAuth2ConnectionInterface $node */
+		$node = $type === 'loader' ? $exchangeSkeleton->getLoader() : $exchangeSkeleton->getExtractor();
+
+		$config = $exchangeConfigStorage->getConfiguration($exchangeSkeleton, $type === 'loader' ? $exchangeSkeleton->getLoader() : $exchangeSkeleton->getExtractor());
+		$node->setConfigCollection($config);
+		try
+		{
+			$authData = $node->getAccessToken($code);
+		}
+		catch (\Exception | \Throwable | ConfigNotFoundException $exception)
+		{
+			return new JsonResponse($exception->getMessage(), 500);
+		}
+		if (isset($authData['error']) && $authData['error'])
+		{
+			return new JsonResponse($authData['error'] . ": " . ($authData['error_description'] ?? null));
+		}
+
+		foreach ($node->getAuthDataMapping() as $configCode => $valueCode)
+		{
+			$exchangeConfigStorage->appendConfiguration($exchangeSkeleton, $node, $configCode, $authData[$valueCode]);
+		}
+
+		return new JsonResponse('ALL OK! Close the window');
+	}
+
+}
---
+++ src/AppBundle/Controller/PreviewController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use iikoExchangeBundle\Application\Period;
+use iikoExchangeBundle\Application\Restaurant;
+use iikoExchangeBundle\Contract\Schedule\ScheduleInterface;
+use iikoExchangeBundle\Exception\ExchangeParameters;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PreviewController extends Controller
+{
+	/**
+	 * @Route("/api/exchange/preview/start")
+	 */
+	public function startExchangePreview(Request $request)
+	{
+
+		$exchange = $this->get('exchange.directory')->getExchangeByCode($request->query->get('exchangeCode'));
+		$exchange->setId($request->query->getInt('exchangeId', 1));
+
+		if ($exchange->getPreviewTemplate() === null)
+		{
+			return new JsonResponse('Exchange doesnt have implementation of preview module', 500);
+		}
+
+		$params = new ExchangeParameters();
+		if ($request->query->has('from') && $request->query->has('to'))
+		{
+			$params->setPeriod(new Period($request->query->get('from'), $request->query->get('to')));
+		}
+		if ($request->query->has('restaurant'))
+		{
+
+			$params->setRestaurant(new Restaurant($request->query->getInt('restaurantId'), 'My restaurant'));
+		}
+
+		if ($this->get('exchange.manager')->startExchange($exchange, ScheduleInterface::TYPE_PREVIEW, $params) !== false)
+		{
+			if ($this->get('exchange.storage.preview')->existData($exchange))
+			{
+				return $this->render("@{$exchange->getPreviewTemplate()}", $this->get('exchange.storage.preview')->getData($exchange));
+			}
+		}
+
+		return new JsonResponse($this->get('exchange.storage.preview')->getError($exchange) ?? 'Unknown error.', 500);
+	}
+}
